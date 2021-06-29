23b47b9e3b0c25f8c52fab0e93c4054b7e19cada
lucas <laksteelhouse@gmail.com>
2021-06-29 11:56:20 +0200

    Updates Cognito with test API

---
+++ README.md
@@ -0,0 +1,1 @@
+Live at https://master.d2sxnpvawjvb80.amplifyapp.com/
--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'eyJraWQiOiJTcHZFNGRsYjhHNGkwbHFkWFJ3Y0NzN1BlQmlXWnJTc1V3NmpnQnlVXC9iUT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiI0ZjlhNDU5MS02ZDBiLTRmNzYtYjMyOC05YmZmNDQ3OWQ1Y2MiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC51cy1lYXN0LTEuYW1hem9uYXdzLmNvbVwvdXMtZWFzdC0xX1lUdFpZeWMybCIsImNvZ25pdG86dXNlcm5hbWUiOiJteS1hdC1lbWFpbC5jb20iLCJvcmlnaW5fanRpIjoiNDEwMTNkMTUtMzRiNS00MWQxLWE4YjAtNzEyMmZkNTE3NDkyIiwiYXVkIjoiNnJsYWphZ284NG92dDk1a29kNWlxbGc3bG0iLCJldmVudF9pZCI6IjhhNTkwZGU5LWJlZjEtNGU4Mi05Mzk1LTc4NjNiNzYwN2I5YiIsInRva2VuX3VzZSI6ImlkIiwiYXV0aF90aW1lIjoxNjI0OTYwNDEwLCJleHAiOjE2MjQ5NjQwMTAsImlhdCI6MTYyNDk2MDQxMCwianRpIjoiNTMwYWI3ZTMtNDhhOC00OTRkLWIwYmUtODQwZDRhMGMwNGViIiwiZW1haWwiOiJteUBlbWFpbC5jb20ifQ.WK7wzlKe0NiPLLfBV1GFqXdwJdkhqb3zJgJZMjLiFEW53GWRhA1tQpErH_Y1Xrh-NvLDMuxEu4oI7EFqlbhz32Xgedl-WsFRN92wqPRjrklf_WqiDbTJ3TSRNf3JZEMYvXeVQNQdcR7vEW29RuhZPLtMB00ilMpDy7a-6ObjT1qZmBLxFjGqSupHJoAZ0VHFjhgPS5j3SV-YvmXlD5zhA9guKm2nO1Vfe-7ZQMgNtHYomqLSXhsmsOdWp7SHQcFaxTnCT3Fgn14dGoxQWPzkkMy0e6UFalJlo9IwlUVDTPa26jfaRl8Sd3voe2Spv0uUUxfU8NL949UKseYNq-aDNw' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
